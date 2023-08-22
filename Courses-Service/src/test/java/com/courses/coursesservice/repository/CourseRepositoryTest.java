e5e53e26833a59f5a6451e310d3b9eecdd1ef5b1
Maximillian Ifeanyi <agwaigbomaximillian@gmail.com>
2023-08-22 12:29:36 +0100

    Added JpaTest For Integration test

--- Courses-Service/src/test/java/com/courses/coursesservice/repository/CourseRepositoryTest.java
+++ Courses-Service/src/test/java/com/courses/coursesservice/repository/CourseRepositoryTest.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
@@ -32,6 +35,29 @@ public class CourseRepositoryTest {
        assertThat(savedCourse.getId()).isNotEqualTo(null);
 
     }
+    @Test
+    @DisplayName("it should return all saved courses and list with size of time ")
+    void getAllCourses(){
+        Courses course = new Courses();
+        course.setPrerequisites("none");
+        course.setCourseTitle("physics");
+        course.setCourseDescription("for 100level students");
+        course.setCourseCode("PHY101");
+        course.setLearningObjectives("All will be present");
+        Courses savedCourse =  courseRepository.save(course);
 
+        Courses course2 = new Courses();
+        course.setPrerequisites("none");
+        course.setCourseTitle("chemistry");
+        course.setCourseDescription("for 100level students");
+        course.setCourseCode("CHM101");
+        course.setLearningObjectives("All will be present");
 
+        Courses savedCourse2 =  courseRepository.save(course2);
+
+       List<Courses> list = courseRepository.findAll();
+
+       assertNotNull(list);
+       assertEquals(12,list.size());
+    }
 }
